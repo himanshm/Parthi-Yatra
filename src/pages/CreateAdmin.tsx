b9799e428b90f6d1ac3dba40884644b89229b33c
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-04-18 20:22:07 +0530

    Hook up Create Admin component with react hook form

--- src/pages/CreateAdmin.tsx
+++ src/pages/CreateAdmin.tsx
@@ -1,14 +1,53 @@
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
+
 import Input from '../components/UI/Input';
 import CreateNewForm from '../components/admin/CreateNewForm';
 
+interface AFormInputs {
+  fullName: string;
+  email: string;
+}
+
 function CreateAdmin() {
+  const { control, handleSubmit, reset } = useForm<AFormInputs>({
+    defaultValues: {
+      fullName: '',
+      email: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<AFormInputs> = (data) => {
+    console.log(data);
+
+    reset();
+  };
   const userInputs = (
     <>
-      <Input type='text' label='Full Name' id='full-name' required />
-      <Input type='email' label='E-mail' id='email' required />
+      <Controller
+        name='fullName'
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => (
+          <Input {...field} type='text' label='Full Name' id='full-name' />
+        )}
+      />
+      <Controller
+        name='email'
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => (
+          <Input {...field} type='email' label='E-mail' id='email' />
+        )}
+      />
     </>
   );
-  return <CreateNewForm inputs={userInputs} entity='admin'></CreateNewForm>;
+  return (
+    <CreateNewForm
+      inputs={userInputs}
+      entity='admin'
+      onSubmit={handleSubmit(onSubmit)}
+    ></CreateNewForm>
+  );
 }
 
 export default CreateAdmin;
--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -2,7 +2,7 @@ type UserForgotPasswordProps = {
   message: string;
 };
 
-function UserForgotPassword({ message }: UserForgotPasswordProps) {
+function ForgotPassword({ message }: UserForgotPasswordProps) {
   return (
     <div className='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700'>
       <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
@@ -13,4 +13,4 @@ function UserForgotPassword({ message }: UserForgotPasswordProps) {
   );
 }
 
-export default UserForgotPassword;
+export default ForgotPassword;
